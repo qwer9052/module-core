14d4817a7560bfc11cc1ea162e9429959a573bde
qwer9052 <qwer9052@gmail.com>
2022-10-31 01:21:23 +0900

    [Add] 유틸 함수 추가

--- src/main/java/com/example/modulecore/util/StringUtil.java
+++ src/main/java/com/example/modulecore/util/StringUtil.java
@@ -1,6 +1,28 @@
 package com.example.modulecore.util;
 
+import org.apache.commons.codec.binary.Hex;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class StringUtil {
+
+    public static String getSHA256( String source ){
+        String SHA256 = "";
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(source.getBytes());
+            byte byteData[] = md.digest();
+            SHA256 = Hex.encodeHexString(byteData);
+            System.out.println("원문 : " + source + " / SHA-256 : " + SHA256);
+        }
+        catch( NoSuchAlgorithmException ex ){
+            ex.printStackTrace();
+            SHA256 = null;
+        }
+        return SHA256;
+    }
+
     public static void test(){
         System.out.println("Adetstset");
     }
