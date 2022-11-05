a52e0267e08a9960a0e81dc7529f0cf701f46094
qwer9052 <qwer9052@gmail.com>
2022-11-06 00:22:06 +0900

    [Add] mapper 추가

--- src/main/java/com/module/core/config/ObjectMapperConfiguration.java
+++ src/main/java/com/module/core/config/ObjectMapperConfiguration.java
@@ -3,6 +3,7 @@ package com.module.core.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +17,10 @@ public class ObjectMapperConfiguration {
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return mapper;
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
